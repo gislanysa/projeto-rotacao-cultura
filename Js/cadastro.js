adbf1cbff61d27265bd6cc77d38728e2fb5b0ee2
juliocesar <juliocmcnha@gmail.com>
2025-05-12 20:17:54 -0300

    Big atualização

---
+++ Js/cadastro.js
@@ -0,0 +1,130 @@
+const mode = ["nome-email", "data-senha"];
+let mode_aux = mode[0];
+
+function nextForm()
+{
+    mode_aux = mode[1];
+
+    const form1 = document.getElementById("form-inicial");
+    form1.classList.remove("form-inicial");
+    form1.classList.add("form-inicial-parse");
+
+    const form2 = document.getElementById("form-final");
+    form2.classList.remove("form-secun-backward");
+    form2.classList.add("form-secun");
+}
+
+function backForm()
+{
+    mode_aux = mode[0];
+    
+    const form1 = document.getElementById("form-inicial");
+    form1.classList.remove("form-inicial-parse");
+    form1.classList.add("form-inicial");
+
+    const form2 = document.getElementById("form-final");
+    form2.classList.remove("form-secun");
+    form2.classList.add("form-secun-backward");
+}
+/*
+var current = null;
+
+document.querySelector('#email').addEventListener('mouseenter', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+document.querySelector('#email').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+document.querySelector('#password').addEventListener('mouseenter', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('mouseenter', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});*/
