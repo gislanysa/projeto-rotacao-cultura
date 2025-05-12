475eac4bce025309993cff318fa8e8ed19455de9
juliocesar <juliocmcnha@gmail.com>
2025-05-12 19:39:33 -0300

    base-estilizada da tela de cadastro

--- Js/cadastro-animation.js
+++ Js/cadastro-animation.js
@@ -1,3 +1,32 @@
+const mode = ["nome-email", "data-senha"];
+let mode_aux = mode[0];
+
+function nextForm()
+{
+    mode_aux = mode[1];
+
+    const form1 = document.getElementById("form-inicial");
+    form1.classList.remove("form-inicial");
+    form1.classList.add("form-inicial-parse");
+
+    const form2 = document.getElementById("form-final");
+    form2.style.display = "flex";
+    form2.classList.remove("form-secun-backward");
+    form2.classList.add("form-secun");
+}
+
+function backForm()
+{
+    mode_aux = mode[0];
+    
+    const form1 = document.getElementById("form-inicial");
+    form1.classList.remove("form-inicial-parse");
+    form1.classList.add("form-inicial");
+
+    const form2 = document.getElementById("form-final");
+    form2.classList.remove("form-secun");
+    form2.classList.add("form-secun-backward");
+}
 /*
 var current = null;
 
@@ -99,4 +128,4 @@ document.querySelector('#submit').addEventListener('focus', function(e) {
       easing: 'easeOutQuart'
     }
   });
-});
+});*/
